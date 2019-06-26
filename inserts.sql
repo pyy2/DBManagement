4301a50bdbdc59345c79d5808376306b9409b6cd
Paul Yu <pyy2@pitt.edu>
2019-06-26 06:33:24 -0400

    start sql insert

---
+++ inserts.sql
@@ -0,0 +1,5 @@
+-- Author
+-- Paul Yu
+insert into store values (1, 'Starbucks', '3618 Forbes Ave', 'Franchise', 40.440917, -79.957609);
+insert into store values (2, 'Red Hawk', '120 Meyran Ave', 'Franchise', 40.440711, -79.958631);
+insert into store values (3, 'Panera Bread', '3800 Forbes Ave', 'Franchise', 40.441697, -79.956547);
